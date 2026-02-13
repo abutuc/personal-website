fc0c2bcc959b88adad4020639d07c31908b275fd
abutuc <andre.butuc@gmail.com>
2026-02-13 19:55:21 +0000

    removed valentine page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NewPage from "./pages/NewPage";
-import Bruna from "./pages/Bruna";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<NewPage />} />
-        <Route path="/bruna" element={<Bruna />} />
       </Routes>
     </Router>
   );
